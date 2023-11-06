96a78fbdc676caec72a3c0c57bb37d9c4af09200
rawatvijay188 <rawatvijay188@gmail.com>
2023-11-06 20:58:12 +0530

    added Report

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,19 @@ import Event from "./Event";
 import NavigationBar from "./NavigationBar";
 import MenuTable from "./menuTable";
 import IngredientTable from "./ingredientsTable";
+import EventDetails from "./Report";
 
 function Home() {
   const [tabValue, setTabValue] = useState(0);
+
+
   return (
     <>
       <NavigationBar setTabValue={setTabValue} />
       <br />
       <div>
-        {tabValue === 0 && <MenuAndIngredients setTabValue={setTabValue} />}
+        {/* {tabValue === 0 && <MenuAndIngredients setTabValue={setTabValue} /> } */}
+        {tabValue === 0 && <EventDetails  />}
         {tabValue === 1 && <Event />}
         {tabValue === 2 && <MenuTable />}
         {tabValue === 3 && <IngredientTable />}
