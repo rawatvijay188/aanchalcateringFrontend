d1a0e7af57cce2b0351afd7e55e123ae4ddfe1b2
Balaji <balaji13.10.1995@gmail.com>
2023-11-10 22:30:24 +0530

    updated home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,24 @@
-import MenuAndIngredients from "./MenuAndIngredients";
 import { useState } from "react";
 import Event from "./Event";
 import NavigationBar from "./NavigationBar";
 import MenuTable from "./menuTable";
 import IngredientTable from "./ingredientsTable";
-<<<<<<< HEAD
 import EventDetails from "./Report";
-
-function Home() {
-  const [tabValue, setTabValue] = useState(0);
-
-
-=======
 import AddEventForm from "./AddEventForm";
 import UpdateEventForm from "./UpdateEventForm";
 import UpdateEventMenuForm from "./UpdateEventMenuForm";
+import MenuAndIngredients from "./MenuAndIngredients";
 
 function Home() {
   const [tabValue, setTabValue] = useState(0);
   const [eventId, setEventId] = useState(null);
->>>>>>> e343d98bb0440006db0bd5f74a9081c09467d9f7
   return (
     <>
       <NavigationBar setTabValue={setTabValue} />
       <br />
-<<<<<<< HEAD
-      <div>
-        {/* {tabValue === 0 && <MenuAndIngredients setTabValue={setTabValue} /> } */}
-        {tabValue === 0 && <EventDetails  />}
-        {tabValue === 1 && <Event />}
-=======
       <div className="container">
         {tabValue === 0 && <MenuAndIngredients setTabValue={setTabValue} />}
         {tabValue === 1 && <Event setTabValue={setTabValue} />}
->>>>>>> e343d98bb0440006db0bd5f74a9081c09467d9f7
         {tabValue === 2 && <MenuTable />}
         {tabValue === 3 && <IngredientTable />}
         {tabValue === 4 && <AddEventForm />}
@@ -41,6 +26,7 @@ function Home() {
           <UpdateEventForm setTabValue={setTabValue} setEventId={setEventId} />
         )}
         {tabValue === 6 && <UpdateEventMenuForm eventId={eventId} />}
+        {tabValue === 7 && <EventDetails  />}
       </div>
     </>
   );
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,34 +4,42 @@ import logo from "../static/images/logo-anchal.png"
 
 function NavigationBar(props) {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-       <Navbar.Brand href="#home">
-            <img
-              src={logo}
-              width="100"
-              height="50"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="100"
+            height="50"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
         </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link
-            onClick={(e) => {
-              e.preventDefault();
-              props.setTabValue(0);
-            }}
-          >
-            Menu
-          </Nav.Link>
-          <Nav.Link onClick={(e) => {
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              onClick={(e) => {
+                e.preventDefault();
+                props.setTabValue(0);
+              }}
+            >
+              Menu
+            </Nav.Link>
+            <Nav.Link onClick={(e) => {
               e.preventDefault();
               props.setTabValue(1);
             }}>Event</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+            <Nav.Link onClick={(e) => {
+              e.preventDefault();
+              props.setTabValue(7);
+            }}>Report</Nav.Link>
+          </Nav>
+
+
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
 
